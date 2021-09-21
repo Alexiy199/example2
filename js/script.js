45a929e208b2c03aaecf35fccd9ff0c3de517c21
alexiy199 <nikitenkoalex1996@icloud.com>
2021-09-21 19:54:51 +0300

    modal

--- js/script.js
+++ js/script.js
@@ -86,3 +86,78 @@ setInterval(() => {
   count++;
   if (count > 3) count = 0;
 }, 6000);
+
+//============= CART ===================================
+const glassesSection = document.querySelector(".glasses"),
+  cartElem = document.querySelector(".cart-count");
+
+let countCart = 0;
+
+const checkClick = function (e) {
+  console.log("check click");
+  if (e.target.dataset.elemgl === "addtocart") {
+    console.log(e.target.children[0].dataset);
+    let blockImgCard = e.srcElement;
+
+    if (e.target.children[0].dataset.add === "added") {
+      console.log("ADDED");
+      e.target.children[0].remove();
+      countCart--;
+      cartElem.innerText = `CART (${countCart})`;
+      blockImgCard.style = "background: #ffa500;";
+      blockImgCard.insertAdjacentHTML(
+        "afterbegin",
+        `<img src="./img/cartpng.png" alt="img" width="25px" height="25px" class="cart">`
+      );
+      return;
+    }
+
+    e.target.children[0].remove();
+
+    blockImgCard.style = "background: #fff;";
+    blockImgCard.insertAdjacentHTML(
+      "afterbegin",
+      `<img src="./img/add.png" alt="img" width="25px" height="25px" data-add="added" class="cart">`
+    );
+    countCart++;
+    cartElem.innerText = `CART (${countCart})`;
+    return;
+  }
+};
+
+const addToCart = (e) => {
+  checkClick(e);
+
+  if (e.target.dataset.elemgl === "cardglasses") {
+    console.log(e.target.children[3].innerText);
+    const imgSrc = e.target.children[0].src;
+
+    const overlay = `<div class="overlay">
+	<div class="discription-product">
+		<span class="close">&#10060;</span>
+		<img src="${imgSrc}" alt="img">
+		<span class="discription-title">Discription</span> <br> <br>
+		<span class="discripton-txt">Здксь доджно быть описание, но это только пример =)</span> 
+	<div class="block-info-more"> 
+		<span>${e.target.children[3].innerText}</span>
+		<div class="box-cart" data-elemgl="addtocart">
+			<img src="./img/cartpng.png" alt="img" width="20px" height="20px" class="cart">
+		</div>
+		</div>
+	</div>
+	</div>`;
+
+    document.body.insertAdjacentHTML("afterbegin", overlay);
+    const blockDiscription = document
+      .querySelector(".discription-product")
+      .addEventListener("click", (evt) => checkClick(evt));
+
+    const close = document
+      .querySelector(".close")
+      .addEventListener("click", () => {
+        console.log("close");
+      });
+  }
+};
+
+glassesSection.addEventListener("click", addToCart);
